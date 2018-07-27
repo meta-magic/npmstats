6a2711e32d89533c5df1481708890e4078688519
medha410 <medha.jadhav@metamagic.in>
2018-07-27 20:02:52 +0530

    Tab Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,163 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  
+  currentDate: any;
+  convertfromdate: any;
+  converttodate: any;
+  lineChartData: any;
+  fromdate: any;
+  todate: any;
+  packagename: any;
+  downloadDataArray :any;
+  other =[]; 
+  array:any=[];
+  downloads=[];
+  disabledDate:any[];
+  errorMessage: any = [];
+  showChart:boolean;
+  isLoading:boolean = false;
+  showflag:boolean;
+  package:string;
+  sum:number=0;
+  isLoading1: boolean;
+  activeTab:boolean;
+  packageModel: PackageModel;
+
+  constructor(private http:HttpClient){
+  this.packageModel = new PackageModel();
+  this.packageModel.packageName="amexio-ng-extensions";
+  
+  }
+
+  ngOnInit()
+  {
+    this.packageModel.fromDate="Jun 1, 2017";
+    this.packageModel.toDate="Jul 31, 2018";
+    this.getData();
+  }
+  
+
+    //THIS METHOD IS USED FOR GET THE DATA FROM SERVICE AND CREATE CHART JSON 
+    getData()
+    {
+     this.sum=0;
+     this.showChart=false;
+     this.isLoading=true;
+     this.errorMessage=[];
+     this.validatePackageName();
+     this.lineChartData=[];
+     let inputUrl: any;
+     this.convertFromDate(this.packageModel.fromDate);
+     this.convertToDate(this.packageModel.toDate);
+     inputUrl = this.convertfromdate + ':' + this.converttodate + '/' + this.packageModel.packageName
+     let response: any;
+     this.http.get('https://api.npmjs.org/downloads/range/' + inputUrl, {}).subscribe(
+       resp => {
+         response = resp;
+         this.showChart= true;
+       
+       },
+       error => {
+   
+                       console.log("GOT ERROR",JSON.stringify(error.error.error));
+                       this.showChart=false;
+                       if(JSON.stringify(error.error.error) == '"end date > start date"')
+                       {
+                          this.errorMessage.push("To-Date should be greater than From-Date")
+                       } else if (JSON.stringify(error.error.error) == '"invalid date"'){
+                         this.errorMessage.push("Enter Valid Date");
+                       } else {
+                         this.errorMessage.push(JSON.stringify(error.error.error));
+ 
+                       }
+                       this.isLoading=false;
+                       this.isLoading1 = false;
+                },
+       () => {
+       
+       
+        this.downloadDataArray = response.downloads;
+         this.lineChartData.push([
+           { "datatype": "string", "label": 'Date' },
+           { "datatype": "number", "label": 'Downloads' }
+         ]);
+             this.downloadDataArray.forEach((downLoadObj:any)=>{
+             let dayWiseDownloadCount:any;
+             dayWiseDownloadCount=new DayWiseDownloadCount(downLoadObj.day,downLoadObj.downloads);
+ 
+            
+             let totaldownload:any;
+             
+             totaldownload=dayWiseDownloadCount.downloads;
+         
+ 
+             this.sum =this.sum+totaldownload;
+             this.lineChartData.push(dayWiseDownloadCount.add());
+             
+         });
+              this.showChart=true;
+              this.isLoading=false;
+              //const totalCount=this.sum;
+       }
+     );
+    
+     this.activeTab=true;
+   }
+ 
+   validatePackageName() {
+    if( this.packageModel.packageName == null || this.packageModel.packageName === '') {
+      this.packageModel.packageName="amexio-ng-extensions"
+    }
+    
+  }
+    
+
+
+  convertFromDate(str: any) {
+      var date = new Date(str),
+      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+      day = ("0" + date.getDate()).slice(-2);
+      this.convertfromdate = [date.getFullYear(), mnth, day].join("-");
+
+  }
+
+  convertToDate(str: any) {
+      var date = new Date(str),
+      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+      day = ("0" + date.getDate()).slice(-2);
+      this.converttodate = [date.getFullYear(), mnth, day].join("-");
+
+  }
+}
+
+export class PackageModel{
+  fromDate : string = '';
+  toDate : string = '';
+  packageName : string = '';
+}
+export class DayWiseDownloadCount {
+  downloads:string;
+  day:number;
+  total = 0;
+
+
+  constructor(day:number,downloads:string){
+     this.day=day;
+     this.downloads=downloads;
+  }
+
+  add():any[]{
+   return [
+    this.day,
+    this.downloads
+    ];
+  }
 }
--- src/app/package-search/package-search.component.ts
+++ src/app/package-search/package-search.component.ts
@@ -1,165 +1,28 @@
-import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
-import { DatePipe } from '@angular/common';
-
-import { HttpModule } from '@angular/http';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Component, OnInit, Pipe, PipeTransform, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-package-search',
+  selector: 'package-search',
   templateUrl: './package-search.component.html',
   styleUrls: ['./package-search.component.css']
 })
 
+
 export class PackageSearchComponent implements OnInit {
 
+  @Input() lineChartData:any;
+  @Input() showChart:boolean;
+  @Input() isLoading:boolean;
  
-  packageModel: PackageModel;
-  currentDate: any;
-  convertfromdate: any;
-  converttodate: any;
-  lineChartData: any;
-  fromdate: any;
-  todate: any;
-  packagename: any;
-  downloadDataArray :any;
-  other =[]; 
-  array:any=[];
-  downloads=[];
-  disabledDate:any[];
-  errorMessage: any = [];
-  showChart:boolean;
-  isLoading:boolean = false;
-  showflag:boolean;
-  package:string;
-  sum:number=0;
-  isLoading1: boolean;
-  
+ 
 
-  constructor(private http: HttpClient) {
+  constructor() {
 
-    this.packageModel = new PackageModel();
+    
    
  }
 
   ngOnInit() {
   }
 
-  validatePackageName() {
-    if( this.packageModel.packageName == null || this.packageModel.packageName === '') {
-      this.packageModel.packageName="amexio-ng-extensions"
-    }
-    
-  }
-    
-  getData()
-   {
-    this.sum=0;
-    this.showChart=false;
-    this.isLoading1 = true;
-    this.isLoading=true;
-    this.errorMessage=[];
-    this.validatePackageName();
-    this.lineChartData=[];
-    let inputUrl: any;
-    this.convertFromDate(this.packageModel.fromDate);
-    this.convertToDate(this.packageModel.toDate);
-    inputUrl = this.convertfromdate + ':' + this.converttodate + '/' + this.packageModel.packageName
-    let response: any;
-    this.http.get('https://api.npmjs.org/downloads/range/' + inputUrl, {}).subscribe(
-      resp => {
-        response = resp;
-        this.showChart= true;
-      
-      },
-      error => {
   
-                      console.log("GOT ERROR",JSON.stringify(error.error.error));
-                      this.showChart=false;
-                      if(JSON.stringify(error.error.error) == '"end date > start date"')
-                      {
-                         this.errorMessage.push("To-Date should be greater than From-Date")
-                      } else if (JSON.stringify(error.error.error) == '"invalid date"'){
-                        this.errorMessage.push("Enter Valid Date");
-                      } else {
-                        this.errorMessage.push(JSON.stringify(error.error.error));
-
-                      }
-                      
-                      this.isLoading=false;
-                      this.isLoading1 = false;
-               },
-      () => {
-      
-      
-       this.downloadDataArray = response.downloads;
-        this.lineChartData.push([
-          { "datatype": "string", "label": 'Date' },
-          { "datatype": "number", "label": 'Downloads' }
-        ]);
-            this.downloadDataArray.forEach((downLoadObj:any)=>{
-            let dayWiseDownloadCount:any;
-            dayWiseDownloadCount=new DayWiseDownloadCount(downLoadObj.day,downLoadObj.downloads);
-
-           
-            let totaldownload:any;
-            
-            totaldownload=dayWiseDownloadCount.downloads;
-        
-
-            this.sum =this.sum+totaldownload;
-            this.lineChartData.push(dayWiseDownloadCount.add());
-            
-        });
-             this.showChart=true;
-             this.isLoading=false;
-             const totalCount=this.sum;
-      }
-    );
-  }
-
-
-  convertFromDate(str: any) {
-      var date = new Date(str),
-      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
-      day = ("0" + date.getDate()).slice(-2);
-      this.convertfromdate = [date.getFullYear(), mnth, day].join("-");
-
-  }
-
-  convertToDate(str: any) {
-      var date = new Date(str),
-      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
-      day = ("0" + date.getDate()).slice(-2);
-      this.converttodate = [date.getFullYear(), mnth, day].join("-");
-
-  }
-}
-export class PackageModel {
-  packageName: string;
-  fromDate: string;
-  toDate: string;
-  constructor() {
-    this.packageName = '';
-    this.fromDate = '';
-    this.toDate = '';
-  }
-}
-
-export class DayWiseDownloadCount {
-  downloads:string;
-  day:number;
-  total = 0;
-
-
-  constructor(day:number,downloads:string){
-     this.day=day;
-     this.downloads=downloads;
-  }
-
-  add():any[]{
-   return [
-    this.day,
-    this.downloads
-    ];
-  }
 }
